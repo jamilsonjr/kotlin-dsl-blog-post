e3b06bd027f31934b33a4196854115e3d07e1536
Jamilson Junior <jamilsonpinheiro.jr@gmail.com>
2024-07-18 22:48:42 +0100

    createTesla function built

--- TeslaShop.kt
+++ TeslaShop.kt
@@ -1,18 +1,24 @@
 
 // Actual code.
 fun main() {
-    // Setup your builder
-	val modelSBuilder = ModelSBuilder()
-    // Customize your tesla
-    modelSBuilder.setDriveMode(DriveMode.ALL_WHEEL_DRIVE)
-	modelSBuilder.setPaint(Paint.STEALTH_GREY)
-    modelSBuilder.setWheels(Wheels.ARACHNID)
-    modelSBuilder.setInterior(Interior.ALL_BLACK)
-    modelSBuilder.setSteering(Steering.YOKE_STEERING)
-    modelSBuilder.setChargingConnector(ChargingConnector.UNIVERSAL_WALL)
-    modelSBuilder.setAcessories(setOf(Acessories.SUNSHADE, Acessories.ALL_WEATHER_INTERIOR_LINERS))
-    modelSBuilder.setSelfDrivingCapiabilities(true)
-    // Build your tesla
-    val modelS = modelSBuilder.build()
+
+   val modelS  = createTesla { builder ->
+       builder.setDriveMode(DriveMode.ALL_WHEEL_DRIVE)
+       builder.setPaint(Paint.STEALTH_GREY)
+       builder.setWheels(Wheels.ARACHNID)
+       builder.setInterior(Interior.ALL_BLACK)
+       builder.setSteering(Steering.YOKE_STEERING)
+       builder.setChargingConnector(ChargingConnector.UNIVERSAL_WALL)
+       builder.setAcessories(setOf(Acessories.SUNSHADE, Acessories.ALL_WEATHER_INTERIOR_LINERS))
+       builder.setSelfDrivingCapiabilities(true)
+   }
+
+
     println (modelS)
+}
+
+fun createTesla(init: (ModelSBuilder) -> Unit): ModelS {
+    val builder = ModelSBuilder()
+    init(builder)
+    return builder.build()
 }
